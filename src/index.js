a6e85bc1f847739828166bb2411b9547ce687a35
Dmytro Semeruk <deemoon83@gmail.com>
2021-08-31 21:29:34 +0300

    мм

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// const getUsersAsync = async () => {
+//     const result = await fetch('http://jsonplaceholder.typicode.com/users');
+//     const dataFromServer = await result.json();
+//     console.log(dataFromServer);
+// }
+
